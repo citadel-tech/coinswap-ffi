d4a67a448faf19e0d5d21e548026bdf1c505f0cc
neoz666 <neoz.blockchain@gmail.com>
2026-01-26 07:02:44 +0530

    Python pip package updates
    Signed-off-by: neoz666 <neoz.blockchain@gmail.com>

---
+++ coinswap-python/build-scripts/release/build-release-macos-aarch64.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+COMPILATION_TARGET="aarch64-apple-darwin"
+RESOURCE_DIR="darwin-arm64"
+LIB_NAME="libcoinswap_ffi.dylib"
+
+echo "Building for target: $COMPILATION_TARGET"
+
+# Move to ffi-commons directory
+cd ../ffi-commons || exit
+rustup target add $COMPILATION_TARGET
+
+# Build the library
+cargo build --profile release-smaller --target $COMPILATION_TARGET
+
+# Copy the binary to the Python native directory
+mkdir -p ../coinswap-python/src/coinswap/native/$RESOURCE_DIR/
+cp ./target/$COMPILATION_TARGET/release-smaller/$LIB_NAME ../coinswap-python/src/coinswap/native/$RESOURCE_DIR/
+cargo run --bin uniffi-bindgen generate --library ./target/$COMPILATION_TARGET/debug/$LIB_NAME --language python --out-dir ../coinswap-python/src/coinswap/native/$RESOURCE_DIR/ --no-format
+
+echo "  Bindings: coinswap-python/src/coinswap/coinswap.py"
+echo "✓ Build completed for $COMPILATION_TARGET"
+echo "  Binary: coinswap-python/src/coinswap/native/$RESOURCE_DIR/$LIB_NAME"
